31d846d4e6f4327ee25cf51254ae331117075b83
FerrariMarc0 <marco1010@live.it>
2023-03-08 16:09:02 +0100

    guests list

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -1,2 +1,23 @@
 'use strict';
 
+const guests= [
+    {name: "Brad", lastName: "Pitt"},
+    {name: "Jhonny", lastName: "Depp"},
+    {name: "Lady", lastName: "Gaga"},
+    {name: "Cristiano", lastName: "Ronaldo"},
+    {name: "Gaetano", lastName: "Frascolla"},
+    {name: "Pupo", lastName: ""},
+    {name: "Little", lastName: "Tony"},
+    {name: "Amedeo", lastName: "Minghi"},
+    {name: "Herbert", lastName: "Ballerina"},
+    {name: "George", lastName: "Clooney",}
+]
+
+console.log(guests);
+
+guests.forEach((element, index) => {
+    console.log(`
+    Il Signor ${element.name} ${element.lastName} siede al tavolo VIP al posto ${index + 1}
+    `);
+
+})
